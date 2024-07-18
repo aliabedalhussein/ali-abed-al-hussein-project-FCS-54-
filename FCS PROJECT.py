7ab0aca13368da01cef4c7563215a777107250a2
unknown <aliabedalhusseinn@gmail.com>
2024-07-18 12:09:38 +0300

    added drive class

---
+++ FCS PROJECT.py
@@ -0,0 +1,5 @@
+class Driver:
+  def __init__(self , driver_id , driver_name , driver_start_city):
+    self.driver_id = driver_id
+    self.driver_name = driver_name
+    self.driver_start_city = driver_start_city

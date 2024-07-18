a65ffbc5273d2c473ea161fc29271fd8e56c5798
unknown <aliabedalhusseinn@gmail.com>
2024-07-18 12:11:33 +0300

    added city class

--- FCS PROJECT.py
+++ FCS PROJECT.py
@@ -3,3 +3,13 @@ class Driver:
     self.driver_id = driver_id
     self.driver_name = driver_name
     self.driver_start_city = driver_start_city
+
+
+class City:
+  def __init__(self , city_name):
+    self.city_name = city_name
+    self.city_neighbours = []
+  
+  def add_neighbour(self , neighbour):
+    if neighbour not in self.city_neighbours:
+      self.city_neighbours.append(neighbour)
